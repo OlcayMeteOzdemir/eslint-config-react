ea6b00113c1d60a0f24147a1e2ad91fb996b8716
Olcay Mete Ozdemir <hey@meteozdemir.com>
2022-11-10 20:48:11 +0300

    Add 'eslint-plugin-prettier' rules

---
+++ rules/prettier/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+	extends: ['@meteozdemir/eslint-config-core/rules/prettier'],
+	plugins: ['prettier'],
+	rules: {
+		'prettier/prettier': 'error',
+
+		'react/jsx-child-element-spacing': 'off',
+		'react/jsx-closing-bracket-location': 'off',
+		'react/jsx-closing-tag-location': 'off',
+		'react/jsx-curly-newline': 'off',
+		'react/jsx-curly-spacing': 'off',
+		'react/jsx-equals-spacing': 'off',
+		'react/jsx-first-prop-new-line': 'off',
+		'react/jsx-indent': 'off',
+		'react/jsx-indent-props': 'off',
+		'react/jsx-max-props-per-line': 'off',
+		'react/jsx-newline': 'off',
+		'react/jsx-one-expression-per-line': 'off',
+		'react/jsx-props-no-multi-spaces': 'off',
+		'react/jsx-tag-spacing': 'off',
+		'react/jsx-wrap-multilines': 'off',
+	},
+};
